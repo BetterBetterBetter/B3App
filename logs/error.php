25cbf4bdda70d15f6f07787c63056d7a8b25c4f5
root <root@betterbetterbetter.us>
2015-07-08 14:52:29 -0400

    first commit

---
+++ logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2015-07-07 14:56:01 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2015-07-07T14:56:01+00:00	INFO 64.134.127.194	joomlafailure	Username and password do not match or you do not have an account yet.
+2015-07-07T14:56:13+00:00	INFO 64.134.127.194	joomlafailure	Username and password do not match or you do not have an account yet.
